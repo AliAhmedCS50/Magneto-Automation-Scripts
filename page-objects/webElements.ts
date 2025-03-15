435cfbf409466b8057a15cb018f8d2768ad2f481
Muhammad Ali <mohdalikareem669@gmail.com>
2025-03-15 16:07:46 +0500

    JS alerts handling

--- page-objects/webElements.ts
+++ page-objects/webElements.ts
@@ -1,14 +1,23 @@
-import {Page, expect} from "@playwright/test"
+import {Page, expect, BrowserContext} from "@playwright/test"
+
+// type MyFixtures = {
+//   context: BrowserContext; // Custom context fixture
+//   page: Page; // Custom page fixture
+
+// };
 
 export class Interactions {
 
   readonly page: Page;
-    constructor(page:Page){
+  readonly context: BrowserContext;
+  
+    constructor(page:Page, ){
 
       this.page = page;
-
+      
     }
 
+// Drop downs     
 async dropdownHandling(){
 
   await this.page.goto('https://artoftesting.com/samplesiteforselenium');
@@ -20,8 +29,98 @@ async dropdownHandling(){
   // i.e await this.page.locator('#testingDropdown').selectOption(index:2);
 
 
+}
+
+async multiSelect(){
+
+  await this.page.goto('https://demoqa.com/select-menu',{ waitUntil: 'load' });
+  await this.page.locator('#cars').selectOption(['Saab', 'Opel', 'Audi']);
+
+}
 
 
+//Handling Alert, confirm alert, and prompt dialogs
+
+//Handling Simple Alert 
+async handleAlertDialogs(){
+
+  await this.page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+  //Interaction with alert
+  this.page.on("dialog", dialog =>{
+    
+    expect(dialog.type()).toEqual("alert");
+    expect(dialog.message()).toEqual("I am a JS Alert");
+    console.log(dialog.type());
+    dialog.accept(); //used to close the dialog 
+  
+
+  })
+   
+  await this.page.getByRole('button',{name:"Click for JS Alert"}).click();
+  //await expect(this.page.locator('#result')).toHaveText('You successfully clicked an alert');
+
+}
+
+
+//Handling confirm dialog
+async confirmDialog(){
+
+  await this.page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+  //await this.page.waitForTimeout(15000);
+
+      this.page.on("dialog", dialog =>{
+        
+        if (dialog.type() === "confirm") {
+          expect(dialog.message()).toEqual("I am a JS Confirm");
+          dialog.accept(); // Accept the confirm dialog
+        } else if (dialog.type() === "alert") {
+          expect(dialog.message()).toEqual("I am a JS Alert");
+          dialog.accept(); // Accept the alert dialog
+        } else {
+          throw new Error(`Unexpected dialog type: ${dialog.type()}`);
+        }
+
+      
+
+      })
+  
+  await this.page.getByRole('button', {name:'Click for JS Confirm'}).click();
+  //await expect(this.page.locator('#result')).toHaveText('You clicked: Cancel');
+
+}
+
+//Handling Prompt Dialog
+async promptDialog(){
+
+  await this.page.goto('https://the-internet.herokuapp.com/javascript_alerts');
+
+      this.page.on("dialog", dialog =>{
+        
+        expect(dialog.type()).toEqual("prompt");
+        expect(dialog.message()).toEqual("I am a JS prompt");
+        dialog.accept("Test Automation by Muhammad Ali"); //used to click OK on the dialog 
+      
+
+      })
+  
+  await this.page.getByRole('button', {name:"Click for JS Prompt"}).click();
+  //await expect(this.page.locator('#result')).toHaveText('You clicked: Cancel');
+
+}
+
+
+async handleNewPage(context){
+
+  const page1 = await context.newPage();
+  await this.page.goto('https://testpages.eviltester.com/styled/index.html');
+  //await expect(page).toHaveTitle('Windows Example Test');
+  const pagePromise = context.waitForEvent(this.page);
+  await this.page.locator('#basicajaxtest').click();
+  const newPage1 = await pagePromise;
+  await this.page.locator("[name = 'submitbutton']").click();
+    
+   
 }
 
 }
--- tests/webInteractions.spec.ts
+++ tests/webInteractions.spec.ts
@@ -1,12 +1,14 @@
-import { test } from "@playwright/test";
+import { test, BrowserContext } from "@playwright/test";
 import {Interactions} from "../page-objects/webElements";
 
 let page;
+let context;
 
 
 test.beforeAll('Automation Practice', async({browser})=>{
 
   page = await browser.newPage();
+  context = await browser.newContext();
   test.setTimeout(300000);
 
 
@@ -17,12 +19,42 @@ test("Handle select dropdown with value and visible text", async() =>{
     const drpdwn = new Interactions(page);
     await drpdwn.dropdownHandling();
     
+});
+
+test("Handle multi select dropdown", async()=>{
+
+    const multi = new Interactions(page);
+    await multi.multiSelect();
+
+})
+
+test("Handle Alert Dialog", async()=>{
+
+    const alert = new Interactions(page);
+    await alert.handleAlertDialogs();
+
+})
+
+test.skip("Handle Confirm Dialog", async()=>{
+
+    const confirmation = new Interactions(page);
+    await confirmation.confirmDialog();
+
+})
+
+test("Handle Prompt Dialog", async()=>{
+
+    const prompt = new Interactions(page);
+    await prompt.promptDialog();
 
 
 });
 
-test("Handle select dropdown with label", async()=>{
 
+test('Handle New Page', async({context})=>{
 
+    const newPage = new Interactions(page);
+    await newPage.handleNewPage(context);
 
 })
+
